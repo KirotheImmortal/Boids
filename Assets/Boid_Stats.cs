156d682b1efd4520fd38c36897915ae447cfc2fa
Quinton Baudoin <baudoin.q@gmail.com>
2015-08-11 17:57:37 -0500

    new repository

---
+++ Assets/Boid_Stats.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class Boid_Stats : MonoBehaviour
+{
+    public bool Flock = false;
+    public bool Preditor = false;     
+    
+    public bool Cohesion  = false;
+    public bool Seperation = false;
+    public bool Allign = false;
+    public bool Boxed = false;
+    public bool Sphered = false;
+
+    public float Velocity_Rng_Min = -5.0f;
+    public float Velocity_Rng_Max = 5.0f;
+
+    public Vector3 velocity;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if(Flock)
+        velocity = new Vector3(Random.Range(Velocity_Rng_Min, Velocity_Rng_Max), Random.Range(Velocity_Rng_Min, Velocity_Rng_Max), Random.Range(Velocity_Rng_Min, Velocity_Rng_Max));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+           // print(gameObject.GetComponent<Transform>().localEulerAngles.x);
+
+            //gameObject.GetComponent<Transform>().rotation = new Quaternion((velocity.normalized.y* -90), (velocity.normalized.z * -90), (velocity.normalized.x * -90), 90); //new Vector4(velocity.normalized.x * 90, (velocity.normalized.x * 90), (velocity.normalized.x * 90), 0); 
+            gameObject.GetComponent<Transform>().position += velocity;
+            gameObject.GetComponent<Boid_Stats>().velocity = (gameObject.GetComponent<Boid_Stats>().velocity.normalized * .15f);
+        
+    }
+
+
+   
+}
---
+++ Assets/Preditor_Con.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Preditor_Con : MonoBehaviour
+{
+    public string up;
+    public string down;
+    public string left;
+    public string right;
+    public string forward;
+    public string back;
+
+   public float Con_Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            gameObject.GetComponent<Boid_Stats>().velocity.y += Con_Speed;
+        if (Input.GetKeyDown(KeyCode.S))
+            gameObject.GetComponent<Boid_Stats>().velocity.y -= Con_Speed;
+        if (Input.GetKeyDown(KeyCode.A))
+            gameObject.GetComponent<Boid_Stats>().velocity.x -= Con_Speed;
+        if (Input.GetKeyDown(KeyCode.D))
+            gameObject.GetComponent<Boid_Stats>().velocity.x += Con_Speed;
+        if (Input.GetKeyDown(KeyCode.I))
+            gameObject.GetComponent<Boid_Stats>().velocity.z += Con_Speed;
+        if (Input.GetKeyDown(KeyCode.K))
+            gameObject.GetComponent<Boid_Stats>().velocity.z -= Con_Speed;
+    }
+
+    
+
+}
